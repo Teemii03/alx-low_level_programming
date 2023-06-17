4dbd88308db3190aef875e54c5588a690d5de1be
Teemii03 <dadatimileyin773@gmail.com>
2023-06-17 08:16:50 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that print a line without printf and puts function
+ * Return: 1
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
