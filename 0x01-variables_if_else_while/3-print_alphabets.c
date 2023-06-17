b1d49c5f4e1b1bb9bc2c86f66b346a84918b501f
Teemii03 <dadatimileyin773@gmail.com>
2023-06-17 12:07:03 -0700

    alPHAbets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print the alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 97;
+	int b = 65;
+
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 90)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
