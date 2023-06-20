942480ca79f08e9dca68aca01bfee574a578d61a
Teemii03 <dadatimileyin773@gmail.com>
2023-06-20 01:34:25 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a script that print putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+	return (0);
+}
